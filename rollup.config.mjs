78bd25eeacec6ac73959f21f2620812d59fa078e
markjoshua12 <74826962+GelamiSalami@users.noreply.github.com>
2023-11-23 23:38:56 +0800

    Added default config builds

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,25 @@ const builds = [
 {
 	input: "src/Toymi.js",
 	output: {
+		file: "build/toymi.module.js",
+		format: "esm"
+	},
+	plugins: [
+	]
+},
+{
+	input: "src/Toymi.js",
+	output: {
+		file: "build/toymi.js",
+		name: "Toymi",
+		format: "cjs"
+	},
+	plugins: [
+	]
+},
+{
+	input: "src/Toymi.js",
+	output: {
 		file: "build/toymi.module.min.js",
 		format: "esm"
 	},
